2009ab90198cc23407e3ebf70709ea5808a4eb65
crlotwhite <crlotwhite@gmail.com>
2024-12-01 20:38:14 +0900

    Refactor g2p function by renaming processing functions for clarity and adding aspiration and liaison rules

--- src/utils/g2p.ts
+++ src/utils/g2p.ts
@@ -1,23 +1,23 @@
 import { ONSET, CODA, VOWEL } from './constant';
 
 function g2p(text: string): string {
-    text = diphthongRunle(text);
-    text = diphthongRunle(text);
-    text = codaSimplification(text);
-    text = clusteredCoda1(text);
-    text = clusteredCoda2(text);
+    text = diphthong(text);
+    text = lenition(text);
+    text = clusterSimplification1(text);
+    text = clusterSimplification2(text);
+    text = aspiration(text);
     return text;
 }
 
 // Rule 5. Diphthong Runle
-function diphthongRunle(text: string): string {
+function diphthong(text: string): string {
     text = text.replaceAll(new RegExp(`(?<=[^${ONSET['ㅇ']}${ONSET['ㄹ']}])${VOWEL['ㅖ']}`, 'g'), VOWEL['ㅔ']);
     text = text.replaceAll(new RegExp(`(?<=[^${ONSET['ㅇ']}])${VOWEL['ㅢ']}`, 'g'), VOWEL['ㅣ']);
     return text;
 }
 
-// Rule 9. Coda Simplification
-function codaSimplification(text: string): string {
+// Rule 9. Lenition of plain stops
+function lenition(text: string): string {
     const pair = [
         { from: `${CODA['ㄲ']}${CODA['ㅋ']}`, to: CODA['ㄱ'] },
         { from: `${CODA['ㅅ']}${CODA['ㅆ']}${CODA['ㅈ']}${CODA['ㅊ']}${CODA['ㅌ']}`, to: CODA['ㄷ'] },
@@ -31,8 +31,8 @@ function codaSimplification(text: string): string {
     return text;
 }
 
-// Rule 10. Clustered Coda1
-function clusteredCoda1(text: string): string {
+// Rule 10. Consonant Cluster Simplification 1
+function clusterSimplification1(text: string): string {
     const pair = [
         { from: `${ONSET['ㅂ']}${VOWEL['ㅏ']}${CODA['ㄼ']}`, to: `${ONSET['ㅂ']}${VOWEL['ㅏ']}${CODA['ㅂ']}` },
         { from: `${ONSET['ㄴ']}${VOWEL['ㅓ']}${CODA['ㄼ']}`, to: `${ONSET['ㄴ']}${VOWEL['ㅓ']}${CODA['ㅂ']}` },
@@ -49,8 +49,8 @@ function clusteredCoda1(text: string): string {
     return text;
 }
 
-// Rule 11. Clustered Coda2
-function clusteredCoda2(text: string): string {
+// Rule 11. Consonant Cluster Simplification 2
+function clusterSimplification2(text: string): string {
     const pair = [
         { from: `${CODA['ㄺ']}`, to: `${CODA['ㄱ']}` },
         { from: `${CODA['ㄻ']}`, to: `${CODA['ㅁ']}` },
@@ -63,20 +63,83 @@ function clusteredCoda2(text: string): string {
 
     return text;
 }
-/*
-1. ‘ㅎ(ㄶ, ㅀ)’ 뒤에 ‘ㄱ, ㄷ, ㅈ’이 결합되는 경우에는, 뒤 음절 첫소리와 합쳐서 [ㅋ, ㅌ, ㅊ]으로 발음한다.
-〔붙임 1〕 받침 ‘ㄱ(ㄺ), ㄷ, ㅂ(ㄼ), ㅈ(ㄵ)’이 뒤 음절 첫소리 ‘ㅎ’과 결합되는 경우에도, 역시 두 소리를 합쳐서 [ㅋ, ㅌ, ㅍ, ㅊ]으로 발음한다.
-〔붙임 2〕 규정에 따라 ‘ㄷ’으로 발음되는 ‘ㅅ, ㅈ, ㅊ, ㅌ’의 경우에는 이에 준한다.
-2. ‘ㅎ(ㄶ, ㅀ)’ 뒤에 ‘ㅅ’이 결합되는 경우에는, ‘ㅅ’을 [ㅆ]으로 발음한다.
-3. ‘ㅎ’ 뒤에 ‘ㄴ’이 결합되는 경우에는, [ㄴ]으로 발음한다.
-〔붙임〕 ‘ㄶ, ㅀ’ 뒤에 뒤에 모음으로 시작된 어미나 접미사가 결합되는 경우에는, ‘ㅎ’을 발음하지 않는다.
-3. ‘ㅎ(ㄶ, ㅀ)’ 뒤에 ‘ㄴ’이 결합되는 경우에는, [ㄴ]으로 발음한다.
 
-*/
-// Rule 12. Coda H
-function codaH(text: string): string {
+// Rule 12. Aspiration of stops
+function aspiration(text: string): string {
+    const group1 = [
+        { from1: CODA['ㅎ'], to1: '' },
+        { from1: CODA['ㄶ'], to1: CODA['ㄴ'] },
+        { from1: CODA['ㅀ'], to1: CODA['ㄹ'] },
+    ]
+    const group2 = [
+        { from2: ONSET['ㄱ'], to2: CODA['ㅋ'] },
+        { from2: ONSET['ㄷ'], to2: CODA['ㅌ'] },
+        { from2: ONSET['ㅈ'], to2: CODA['ㅊ'] },
+        { from2: ONSET['ㅅ'], to2: CODA['ㅆ'] },
+    ]
+
+    for (const { from1, to1 } of group1) {
+        for (const { from2, to2 } of group2) {
+            text = text.replaceAll(new RegExp(`${from1}${from2}`, 'g'), `${to1}${to2}`);
+        }
+    }
+
+    const pair = [
+        { from: `${CODA['ㄱ']}`, to: ONSET['ㅋ'] },
+        { from: `${CODA['ㄷ']}`, to: ONSET['ㅌ'] },
+        { from: `${CODA['ㅂ']}`, to: ONSET['ㅍ'] },
+        { from: `${CODA['ㅈ']}`, to: ONSET['ㅊ'] },
+        { from: `${CODA['ㄺ']}`, to: `${CODA['ㄹ']}${ONSET['ㅋ']}` },
+        { from: `${CODA['ㄼ']}`, to: `${CODA['ㄹ']}${ONSET['ㅍ']}` },
+        { from: `${CODA['ㄵ']}`, to: `${CODA['ㄴ']}${ONSET['ㅊ']}` },
+    ]
+
+    for (const { from, to } of pair) {
+        text = text.replaceAll(new RegExp(`${from}${ONSET['ㅎ']}`, 'g'), to);
+    }
+
+    text = text.replaceAll(new RegExp(`${CODA['ㅎ']}(?:(?=${ONSET['ㄴ']})|(?=${ONSET['ㅇ']}))`, 'g'), CODA['ㄴ']);
+    text = text.replaceAll(new RegExp(`${CODA['ㄶ']}(?:(?=${ONSET['ㄴ']})|(?=${ONSET['ㅇ']}))`, 'g'), CODA['ㄴ']);
+    text = text.replaceAll(new RegExp(`${CODA['ㅀ']}(?:(?=${ONSET['ㄴ']})|(?=${ONSET['ㅇ']}))`, 'g'), CODA['ㄹ']);
+
+
+    for (const { from, to } of pair) {
+        text = text.replaceAll(new RegExp(`${from}(?!${ONSET['ㅇ']})|${from}$|${from}(?=[\\s\\.\\!\\?])`, 'g'), to);
+    }
+
     return text;
 }
+
+// Rule 13. Liaison Rule
+function liaison(text: string): string {
+    const codas = Object.keys(CODA);
+    const onsets = Object.keys(ONSET);
+
+    for (const coda of codas) {
+        if (onsets.includes(coda)) {
+            text = text.replaceAll(new RegExp(`${CODA[coda]}${ONSET['ㅇ']}`, 'g'), ONSET[coda]);
+        }
+    }
+
+    const pair = [
+        { from: CODA['ㄳ'], to: `${CODA['ㄱ']}${ONSET['ㅆ']}` },
+        { from: CODA['ㄵ'], to: `${CODA['ㄴ']}${ONSET['ㅈ']}` },
+        { from: CODA['ㄺ'], to: `${CODA['ㄹ']}${ONSET['ㄱ']}` },
+        { from: CODA['ㄻ'], to: `${CODA['ㄹ']}${ONSET['ㅁ']}` },
+        { from: CODA['ㄽ'], to: `${CODA['ㄹ']}${ONSET['ㅆ']}` },
+        { from: CODA['ㄾ'], to: `${CODA['ㄹ']}${ONSET['ㅌ']}` },
+        { from: CODA['ㄿ'], to: `${CODA['ㄹ']}${ONSET['ㅍ']}` },
+        { from: CODA['ㅄ'], to: `${CODA['ㅂ']}${ONSET['ㅆ']}` },
+    ];
+
+    for (const { from, to } of pair) {
+        text = text.replaceAll(new RegExp(`${from}${ONSET['ㅇ']}`, 'g'), to);
+    }
+
+    return text;
+}
+
+// Ru
 
 // Rule Tensification
 
