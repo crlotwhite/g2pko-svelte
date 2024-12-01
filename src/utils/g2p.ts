660f8b3db76580f7ff1926100458f202aafcdbbf
crlotwhite <crlotwhite@gmail.com>
2024-12-02 00:02:27 +0900

    Refactor g2p function to streamline processing steps and enhance readability with logging

--- src/utils/g2p.ts
+++ src/utils/g2p.ts
@@ -2,20 +2,29 @@ import { ONSET, CODA, VOWEL } from './constant';
 import { decomposeAll } from './common';
 
 function g2p(text: string): string {
-    text = exceptionWords(text);
-    text = diphthong(text);
-    text = liaison(text);
-    text = palatalization(text);
-    text = liquidToNasalAssimilation(text);
-    text = nasalization(text);
-    text = nasalInsertion(text);
-    text = lateralization(text);
-    text = tensification(text);
-    text = epenthesis(text);
-    text = lenition(text);
-    text = clusterSimplification1(text);
-    text = clusterSimplification2(text);
-    text = aspiration(text);
+    const process = [
+        exceptionWords,
+        diphthong,
+        palatalization,
+        liquidToNasalAssimilation,
+        epenthesis,
+        nasalization,
+        nasalInsertion,
+        lateralization,
+        tensification,
+        liaison,
+        lenition,
+        clusterSimplification1,
+        clusterSimplification2,
+        aspiration,
+    ];
+
+    for (const p of process) {
+        const before = text;
+        text = p(text);
+        console.log(`[Process] ${p.name} ${before} -> ${text}`);
+    }
+
     return text;
 }
 
@@ -51,8 +60,8 @@ function diphthong(text: string): string {
 // Rule 9. Lenition of plain stops
 function lenition(text: string): string {
     const pair = [
-        { from: `${CODA['ㄲ']}${CODA['ㅋ']}`, to: CODA['ㄱ'] },
-        { from: `${CODA['ㅅ']}${CODA['ㅆ']}${CODA['ㅈ']}${CODA['ㅊ']}${CODA['ㅌ']}`, to: CODA['ㄷ'] },
+        { from: `[${CODA['ㄲ']}${CODA['ㅋ']}]`, to: CODA['ㄱ'] },
+        { from: `[${CODA['ㅅ']}${CODA['ㅆ']}${CODA['ㅈ']}${CODA['ㅊ']}${CODA['ㅌ']}]`, to: CODA['ㄷ'] },
         { from: `${CODA['ㅍ']}`, to: CODA['ㅂ'] },
     ];
 
@@ -70,8 +79,8 @@ function clusterSimplification1(text: string): string {
         { from: `${ONSET['ㄴ']}${VOWEL['ㅓ']}${CODA['ㄼ']}`, to: `${ONSET['ㄴ']}${VOWEL['ㅓ']}${CODA['ㅂ']}` },
         { from: `${CODA['ㄳ']}`, to: CODA['ㄱ'] },
         { from: `${CODA['ㄵ']}`, to: CODA['ㄴ'] },
-        { from: `${CODA['ㄼ']}${CODA['ㄽ']}${CODA['ㄾ']}`, to: CODA['ㄹ'] },
-        { from : `${CODA['ㅄ']}`, to: CODA['ㅂ'] },
+        { from: `[${CODA['ㄼ']}${CODA['ㄽ']}${CODA['ㄾ']}]`, to: CODA['ㄹ'] },
+        { from: `${CODA['ㅄ']}`, to: CODA['ㅂ'] },
     ];
 
     for (const { from, to } of pair) {
@@ -104,10 +113,10 @@ function aspiration(text: string): string {
         { from1: CODA['ㅀ'], to1: CODA['ㄹ'] },
     ]
     const group2 = [
-        { from2: ONSET['ㄱ'], to2: CODA['ㅋ'] },
-        { from2: ONSET['ㄷ'], to2: CODA['ㅌ'] },
-        { from2: ONSET['ㅈ'], to2: CODA['ㅊ'] },
-        { from2: ONSET['ㅅ'], to2: CODA['ㅆ'] },
+        { from2: ONSET['ㄱ'], to2: ONSET['ㅋ'] },
+        { from2: ONSET['ㄷ'], to2: ONSET['ㅌ'] },
+        { from2: ONSET['ㅈ'], to2: ONSET['ㅊ'] },
+        { from2: ONSET['ㅅ'], to2: ONSET['ㅆ'] },
     ]
 
     for (const { from1, to1 } of group1) {
@@ -133,11 +142,6 @@ function aspiration(text: string): string {
     text = text.replaceAll(new RegExp(`${CODA['ㅎ']}(?:(?=${ONSET['ㄴ']})|(?=${ONSET['ㅇ']}))`, 'g'), CODA['ㄴ']);
     text = text.replaceAll(new RegExp(`${CODA['ㄶ']}(?:(?=${ONSET['ㄴ']})|(?=${ONSET['ㅇ']}))`, 'g'), CODA['ㄴ']);
     text = text.replaceAll(new RegExp(`${CODA['ㅀ']}(?:(?=${ONSET['ㄴ']})|(?=${ONSET['ㅇ']}))`, 'g'), CODA['ㄹ']);
-
-
-    for (const { from, to } of pair) {
-        text = text.replaceAll(new RegExp(`${from}(?!${ONSET['ㅇ']})|${from}$|${from}(?=[\\s\\.\\!\\?])`, 'g'), to);
-    }
 
     return text;
 }
